1736897f773063d6093470eb5730a9de71a97b8f
LuyenNDTSU-PTON-JV241226 <dinhluyen2402@gmail.com>
2025-03-20 23:23:49 +0900

    upload btvn module2 ss01-2

---
+++ ss01-2.js
@@ -0,0 +1,11 @@
+const checkParity = (a) => {
+    if(isNaN (a)){
+        console.log(a, " not is number");
+    }else if(a % 2 === 0){
+         console.log(a, " is an even number");
+    }else{
+        console.log(a, " is odd");
+    };
+    return a;
+}
+checkParity("s12");
